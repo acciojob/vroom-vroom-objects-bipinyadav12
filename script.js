b395bc25412b59f3b05ca5d90f4d480a8fcd78f4
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-12-03 15:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Car(make, model) {
 	this.modal=model
 }
 Car.prototype.getMakeModel= function () {
-	return `${this.make}${this.model}`
+	return `${this.make} ${this.model}`
 }
 
 function SportsCar(make, model, topSpeed) {
@@ -16,7 +16,9 @@ SportsCar.prototype.getTopSpeed=function () {
 	return this.topSpeed
 }
 Object.setPrototypeOf(SportsCar.prototype,Car.prototype)
-
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); 
+console.log(car.getTopSpeed());
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
