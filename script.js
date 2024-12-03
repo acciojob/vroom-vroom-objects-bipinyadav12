2387bbce291fa0427808b68c6f4c939fac8a63e2
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-12-03 15:29:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,13 +11,12 @@ function SportsCar(make, model, topSpeed) {
 	this.model=model
 	this.topSpeed=topSpeed
 }
-SportsCar.prototype.getMakeModel= function () {
-	return `${this.make}${this.model}`
-}
+
 SportsCar.prototype.getTopSpeed=function () {
 	return this.topSpeed
 }
-SportsCar.__proto__=Car
+Object.setPrototypeOf(SportsCar.prototype,Car.prototype)
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
